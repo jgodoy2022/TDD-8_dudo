2d9ac672a6ee9224f9acb569172bf38f110958f2
jgodoy2022 <jgodoy2022@udec.cl>
2025-08-21 12:23:58 -0400

    GREEN: implementacion de mock para resultado determinista en los dados

--- tests/test_dado.py
+++ tests/test_dado.py
@@ -1,14 +1,19 @@
 import unittest
+from unittest.mock import patch
 from src.juego.dado import Dado
 
-def test_dado_valor():
+@patch('src.juego.dado.random.randint', return_value=2)
+def test_dado_valor(mock_valor):
     dado=Dado()
     v_validos=[1,2,3,4,5,6]
     valor=dado.tirar()
+    assert valor ==2
     assert valor in v_validos
 
-def test_dado_deno():
+@patch('src.juego.dado.random.randint', return_value=3)
+def test_dado_deno(mock_deno):
     dado=Dado()
     deno_validos=["As","Tonto","Tren","Cuadra","Quina","Sexto"]
     valor=dado.tirar()
+    assert dado.deno() =="Tren"
     assert dado.deno() in deno_validos
