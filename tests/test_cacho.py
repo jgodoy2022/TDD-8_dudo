5ba9da0e4892f98b07b60bbe85d1de330729dc72
jgodoy2022 <jgodoy2022@udec.cl>
2025-08-22 16:53:28 -0400

    RED: test para agitar el cacho y generar valores en los dados, falla como esperado

--- tests/test_cacho.py
+++ tests/test_cacho.py
@@ -1,5 +1,15 @@
+from unittest.mock import patch
 from src.juego.cacho import Cacho
+from src.juego.dado import Dado
 
 def test_cacho_inicio():
     cacho = Cacho()
     assert len(cacho.dados) == 5
+
+def test_agitar():
+    v_validos = [1, 2, 3, 4, 5, 6]
+    cacho=Cacho()
+    cacho.agitar()
+
+    for dado in cacho.dados:
+        assert dado.valor in v_validos
