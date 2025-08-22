aed3e771205e825916d305d845990b646e36a8f5
jgodoy2022 <jgodoy2022@udec.cl>
2025-08-22 17:24:53 -0400

    REFACTOR: Se implemento la posibilidad de definir la cantidad de dados que tendra el cacho inicialmente

--- src/juego/cacho.py
+++ src/juego/cacho.py
@@ -3,9 +3,9 @@ from src.juego.dado import Dado
 class Cacho:
 
     # Inicializa el cacho con 5 dados
-    def __init__(self):
+    def __init__(self, cantidad_dados=5):
         self.dados = []
-        for _ in range(5):
+        for _ in range(cantidad_dados):
             self.dados.append(Dado())
 
     # Emplea la funcion tirar para cada dado
