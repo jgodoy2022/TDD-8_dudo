c59e6219502b2f0a0dbd90a1b365a669807c4799
jgodoy2022 <jgodoy2022@udec.cl>
2025-08-22 16:57:01 -0400

    GREEN: implementacion de la funcionalidad agitar para generar los valores de cada dado dentro del cacho

--- src/juego/cacho.py
+++ src/juego/cacho.py
@@ -1,9 +1,16 @@
 from src.juego.dado import Dado
 
 class Cacho:
+
+    # Inicializa el cacho con 5 dados
     def __init__(self):
         self.dados = []
         for _ in range(5):
             self.dados.append(Dado())
+
+    # Emplea la funcion tirar para cada dado
+    def agitar(self):
+        for dado in self.dados:
+            dado.tirar()
 
 
