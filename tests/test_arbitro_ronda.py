96c6b11d95b2825e0f3c3f481a6c24590a4996d7
unknown <marcvergara2023@udec.cl>
2025-08-31 19:32:56 -0400

    RED: test para determinar resultado de duda - falla como esperado

---
+++ tests/test_arbitro_ronda.py
@@ -0,0 +1,29 @@
+import pytest
+import sys
+sys.path.append('')
+from src.juego.arbitro_ronda import ArbitroRonda
+from src.juego.cacho import Cacho
+from src.juego.dado import Dado
+from unittest.mock import patch
+
+@patch('src.juego.dado.random.randint', return_value = 3)
+def test_resultado_duda():
+    cachos = []
+    for i in range(2):
+        cachos.append(Cacho(2).agitar)
+    arbitro = ArbitroRonda()
+
+    assert arbitro.dudar(cachos, 3, "Tren") is False
+    assert arbitro.dudar(cachos, 2, "Tonto") is True
+
+
+    cachos[0].dados[0].valor = 2
+    assert arbitro.dudar(cachos, 4, "Tren") is True
+    assert arbitro.dudar(cachos, 1, "Tonto") is False
+    assert arbitro.dudar(cachos, 3, "Tonto") is True
+
+    cachos[0].dados[1].valor = 1
+    assert arbitro.dudar(cachos, 3, "Tren") is False
+    assert arbitro.dudar(cachos, 4, "Tren") is True
+    assert arbitro.dudar(cachos, 2, "Tonto") is False
+    assert arbitro.dudar(cachos, 4, "Tonto") is True
