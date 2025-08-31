132ccff54aa5da360a90a9193394ec080a047e6c
unknown <marcvergara2023@udec.cl>
2025-08-31 19:52:14 -0400

    RED: test para determinar resultado de calzar - falla como esperado

--- tests/test_arbitro_ronda.py
+++ tests/test_arbitro_ronda.py
@@ -29,3 +29,30 @@ def test_resultado_duda(mock_valor):
     assert arbitro.dudar(cachos, 4, "Tren") is True
     assert arbitro.dudar(cachos, 2, "Tonto") is False
     assert arbitro.dudar(cachos, 4, "Tonto") is True
+
+@patch('src.juego.dado.random.randint', return_value = 3)
+def test_resultado_calzar(mock_valor):
+    cachos = []
+    for i in range(2):
+        c = Cacho(2)
+        c.agitar()
+        cachos.append(c)
+    arbitro = ArbitroRonda()
+
+    assert arbitro.calzar(cachos, 3, "Tren") is False
+    assert arbitro.calzar(cachos, 4, "Tren") is True
+    assert arbitro.calzar(cachos, 2, "Tonto") is False
+
+
+    cachos[0].dados[0].valor = 2
+    assert arbitro.calzar(cachos, 4, "Tren") is False
+    assert arbitro.calzar(cachos, 3, "Tren") is True
+    assert arbitro.calzar(cachos, 1, "Tonto") is True
+    assert arbitro.calzar(cachos, 3, "Tonto") is False
+
+    cachos[0].dados[1].valor = 1
+    assert arbitro.calzar(cachos, 3, "Tren") is True
+    assert arbitro.calzar(cachos, 2, "Tren") is False
+    assert arbitro.calzar(cachos, 2, "Tonto") is True
+    assert arbitro.calzar(cachos, 1, "Tonto") is False
+    assert arbitro.calzar(cachos, 1, "As") is True
