78c6e0e0386641ae3dd6db418a8fd0d9f121e064
unknown <marcvergara2023@udec.cl>
2025-08-31 21:53:48 -0400

    GREEN: implementación mínima para permitir calzar al jugador con un dado

--- src/juego/arbitro_ronda.py
+++ src/juego/arbitro_ronda.py
@@ -15,12 +15,15 @@ class ArbitroRonda:
         return self.contador.contar(pinta, cachos) < cantidad
     
     # retorna true si la cantidad de pintas en la apuesta coincide exactamente con la cantidad de pintas en los dados y false si no lo hace
-    def calzar(self, cachos, cantidad, pinta):
+    def calzar(self, cachos, cantidad, pinta, cacho_calzador):
+        ronda_especial = False
+        if len(cacho_calzador.dados) == 1:
+            ronda_especial = True
         cant_dados = 0
         for c in cachos:
             cant_dados += len(c.dados)
         mitad = (len(cachos)*5)/2
-        if cant_dados < mitad:
-            exc = CalzarInvalido(f"No se puede calzar, hay menos de {int(mitad)} dados")
+        if cant_dados < mitad and not ronda_especial:
+            exc = CalzarInvalido(f"No se puede calzar, hay menos de {int(mitad)} dados y el jugador que calza tiene más de un dado")
             raise exc
         return self.contador.contar(pinta, cachos) == cantidad
