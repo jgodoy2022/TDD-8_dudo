97bb8aa9b72ce1da3937109e7051cbf7654990c1
unknown <marcvergara2023@udec.cl>
2025-08-31 21:36:35 -0400

    RED: test para revisar si es válido calzar

--- tests/test_arbitro_ronda.py
+++ tests/test_arbitro_ronda.py
@@ -1,9 +1,8 @@
 import pytest
 import sys
 sys.path.append('')
-from src.juego.arbitro_ronda import ArbitroRonda
+from src.juego.arbitro_ronda import ArbitroRonda, CalzarInvalido
 from src.juego.cacho import Cacho
-from src.juego.dado import Dado
 from unittest.mock import patch
 
 @patch('src.juego.dado.random.randint', return_value = 3)
@@ -34,25 +33,37 @@ def test_resultado_duda(mock_valor):
 def test_resultado_calzar(mock_valor):
     cachos = []
     for i in range(2):
-        c = Cacho(2)
+        c = Cacho(3)
         c.agitar()
         cachos.append(c)
     arbitro = ArbitroRonda()
 
     assert arbitro.calzar(cachos, 3, "Tren") is False
-    assert arbitro.calzar(cachos, 4, "Tren") is True
+    assert arbitro.calzar(cachos, 6, "Tren") is True
     assert arbitro.calzar(cachos, 2, "Tonto") is False
 
 
     cachos[0].dados[0].valor = 2
     assert arbitro.calzar(cachos, 4, "Tren") is False
-    assert arbitro.calzar(cachos, 3, "Tren") is True
+    assert arbitro.calzar(cachos, 5, "Tren") is True
     assert arbitro.calzar(cachos, 1, "Tonto") is True
     assert arbitro.calzar(cachos, 3, "Tonto") is False
 
     cachos[0].dados[1].valor = 1
-    assert arbitro.calzar(cachos, 3, "Tren") is True
-    assert arbitro.calzar(cachos, 2, "Tren") is False
+    assert arbitro.calzar(cachos, 5, "Tren") is True
+    assert arbitro.calzar(cachos, 4, "Tren") is False
     assert arbitro.calzar(cachos, 2, "Tonto") is True
     assert arbitro.calzar(cachos, 1, "Tonto") is False
     assert arbitro.calzar(cachos, 1, "As") is True
+
+@patch('src.juego.dado.random.randint', return_value = 3)
+def test_es_valido_calzar(mock_valor):
+    cachos = []
+    for i in range(2):
+        c = Cacho(2)
+        c.agitar()
+        cachos.append(c)
+    arbitro = ArbitroRonda()
+
+    with pytest.raises(CalzarInvalido):
+        arbitro.calzar(cachos, 2, "Tren")
