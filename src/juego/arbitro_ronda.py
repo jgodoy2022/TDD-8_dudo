a7ecf079815aa3e3a2a42a16d913597b8a0bd37e
unknown <marcvergara2023@udec.cl>
2025-08-31 21:38:01 -0400

    GREEN: implementación mínima para revisar si es válido calzar

--- src/juego/arbitro_ronda.py
+++ src/juego/arbitro_ronda.py
@@ -2,6 +2,10 @@ import sys
 sys.path.append('')
 from src.juego.contador_pintas import ContadorPintas
 
+class CalzarInvalido(Exception):
+    def __init__(self, mensaje):
+        super().__init__(mensaje)
+
 class ArbitroRonda:
     def __init__(self):
         self.contador = ContadorPintas()
@@ -12,4 +16,11 @@ class ArbitroRonda:
     
     # retorna true si la cantidad de pintas en la apuesta coincide exactamente con la cantidad de pintas en los dados y false si no lo hace
     def calzar(self, cachos, cantidad, pinta):
+        cant_dados = 0
+        for c in cachos:
+            cant_dados += len(c.dados)
+        mitad = (len(cachos)*5)/2
+        if cant_dados < mitad:
+            exc = CalzarInvalido(f"No se puede calzar, hay menos de {int(mitad)} dados")
+            raise exc
         return self.contador.contar(pinta, cachos) == cantidad
