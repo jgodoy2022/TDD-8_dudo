bd1c37a06f827177bcbd837ddd3ae646752650a2
unknown <marcvergara2023@udec.cl>
2025-09-02 22:32:46 -0400

    RED: test para el resultado de dudar cuando los ases no son comodín - falla como esperado

--- tests/test_arbitro_ronda.py
+++ tests/test_arbitro_ronda.py
@@ -5,7 +5,8 @@ from src.juego.arbitro_ronda import ArbitroRonda, CalzarInvalido
 from src.juego.cacho import Cacho
 from unittest.mock import patch
 
-
+# Si el resultado de dudar() es True, la duda fue válida y pierde quien apostó,
+# si el resultado de dudar() es False, la duda fue equívoca y pierde quien dudó
 @patch('src.juego.dado.random.randint', return_value=3)
 def test_resultado_duda(mock_valor):
     # Configura dos cachos con dados fijos para test de dudar
@@ -26,13 +27,22 @@ def test_resultado_duda(mock_valor):
     assert arbitro.dudar(cachos, 1, "Tonto") is False
     assert arbitro.dudar(cachos, 3, "Tonto") is True
 
+    # Combinando comodines
     cachos[0].dados[1].valor = 1
     assert arbitro.dudar(cachos, 3, "Tren") is False
     assert arbitro.dudar(cachos, 4, "Tren") is True
     assert arbitro.dudar(cachos, 2, "Tonto") is False
     assert arbitro.dudar(cachos, 4, "Tonto") is True
 
+    # Ronda Especial (As no es comodín)
+    ronda_especial = True
+    assert arbitro.dudar(cachos, 3, "Tren", ronda_especial) is True
+    assert arbitro.dudar(cachos, 2, "Tren", ronda_especial) is False
+    assert arbitro.dudar(cachos, 2, "Tonto", ronda_especial) is True
+    assert arbitro.dudar(cachos, 1, "Tonto", ronda_especial) is False
 
+# Si el resultado de calzar() es True, el jugador que calza acertó y gana un dado
+# si el resultado de calzar() es False, el jugador que calza se equivocó y pierde un dado
 @patch('src.juego.dado.random.randint', return_value=3)
 def test_resultado_calzar(mock_valor):
     # Configura cachos para test de calzar
@@ -62,6 +72,14 @@ def test_resultado_calzar(mock_valor):
     assert arbitro.calzar(cachos, 2, "Tonto", cachos[0]) is True
     assert arbitro.calzar(cachos, 1, "Tonto", cachos[0]) is False
     assert arbitro.calzar(cachos, 1, "As", cachos[0]) is True
+
+    # Ronda Especial (As no es comodín)
+    ronda_especial = True
+    assert arbitro.calzar(cachos, 5, "Tren", cachos[0], ronda_especial) is False
+    assert arbitro.calzar(cachos, 4, "Tren", cachos[0], ronda_especial) is True
+    assert arbitro.calzar(cachos, 2, "Tonto", cachos[0], ronda_especial) is False
+    assert arbitro.calzar(cachos, 1, "Tonto", cachos[0], ronda_especial) is True
+    assert arbitro.calzar(cachos, 1, "As", cachos[0], ronda_especial) is True
 
 
 @patch('src.juego.dado.random.randint', return_value=3)
