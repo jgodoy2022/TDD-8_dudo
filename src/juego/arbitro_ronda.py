4b9cdf5ad672587811297a7156b112d6bfa8498e
unknown <marcvergara2023@udec.cl>
2025-09-02 22:34:03 -0400

    GREEN: implementación mínima para el resultado de dudar cuando los ases no son comodín

--- src/juego/arbitro_ronda.py
+++ src/juego/arbitro_ronda.py
@@ -11,19 +11,19 @@ class ArbitroRonda:
         self.contador = ContadorPintas()
 
     # retorna true si la duda fue correcta (la apuesta se pasó) y false si la duda fue incorrecta
-    def dudar(self, cachos, cantidad, pinta):
-        return self.contador.contar(pinta, cachos) < cantidad
+    def dudar(self, cachos, cantidad, pinta, ronda_especial=False):
+        return self.contador.contar(pinta, cachos, ronda_especial) < cantidad
     
     # retorna true si la cantidad de pintas en la apuesta coincide exactamente con la cantidad de pintas en los dados y false si no lo hace
-    def calzar(self, cachos, cantidad, pinta, cacho_calzador):
-        ronda_especial = False
+    def calzar(self, cachos, cantidad, pinta, cacho_calzador, ronda_especial=False):
+        jugador_valido = False
         if len(cacho_calzador.dados) == 1:
-            ronda_especial = True
+            jugador_valido = True
         cant_dados = 0
         for c in cachos:
             cant_dados += len(c.dados)
         mitad = (len(cachos)*5)/2
-        if cant_dados < mitad and not ronda_especial:
+        if cant_dados < mitad and not jugador_valido:
             exc = CalzarInvalido(f"No se puede calzar, hay menos de {int(mitad)} dados y el jugador que calza tiene más de un dado")
             raise exc
-        return self.contador.contar(pinta, cachos) == cantidad
+        return self.contador.contar(pinta, cachos, ronda_especial) == cantidad
